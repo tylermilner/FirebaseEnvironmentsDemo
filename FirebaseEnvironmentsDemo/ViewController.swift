b9c4a4b1654e42d9f4cef17517e3f99a53ef6b1b
Tyler Milner <tyler.milner@bottlerocketstudios.com>
2017-07-28 11:58:53 -0500

    Created Xcode project.

---
+++ FirebaseEnvironmentsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseEnvironmentsDemo
+//
+//  Created by Tyler Milner on 7/28/17.
+//  Copyright © 2017 Bottle Rocket Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
